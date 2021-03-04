14d43f5e21150b21270cbe928ae87eed68908a75
shankar <mukthas01@gmail.com>
2021-03-04 17:14:01 +0530

    commit

---
+++ sss/sss/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  sss
+//
+//  Created by M Shankar on 3/4/21.
+//  Copyright © 2021 shankarm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("shankar")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
